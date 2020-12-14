79e380cadbf9a7e21a963b26a171cce82ac6e4ee
PeachBlack-Alba <peachblackweb@gmail.com>
2020-12-14 13:26:45 +0100

    done

--- lib/Home.dart
+++ lib/Home.dart
@@ -14,6 +14,7 @@ class _HomeState extends State<Home> {
   DateTime startTime = DateTime.now();
   DateTime endTime = DateTime.now().add(Duration(days: 1));
   TextEditingController _eventName = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,74 +24,91 @@ class _HomeState extends State<Home> {
 
   _body(BuildContext context) {
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              FlatButton(
-                  onPressed: () {
-                    DatePicker.showDateTimePicker(context,
-                        showTitleActions: true,
-                        minTime: DateTime(2019, 3, 5),
-                        maxTime: DateTime(2200, 6, 7), onChanged: (date) {
-                          print('change $date');
-                        }, onConfirm: (date) {
-                          setState(() {
-                            this.startTime = date;
-                          });
-                        }, currentTime: DateTime.now(), locale: LocaleType.en);
-                  },
-                  child: Text(
-                    'Event Start Time',
-                    style: TextStyle(color: Colors.blue),
-                  )),
-              Text('$startTime'),
-            ],
-          ),
-          Row(
-            children: <Widget>[
-              FlatButton(
-                  onPressed: () {
-                    DatePicker.showDateTimePicker(context,
-                        showTitleActions: true,
-                        minTime: DateTime(2019, 3, 5),
-                        maxTime: DateTime(2200, 6, 7), onChanged: (date) {
-                          print('change $date');
-                        }, onConfirm: (date) {
-                          setState(() {
-                            this.endTime = date;
-                          });
-                        }, currentTime: DateTime.now(), locale: LocaleType.en);
-                  },
-                  child: Text(
-                    'Event End Time',
-                    style: TextStyle(color: Colors.blue),
-                  )),
-              Text('$endTime'),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: TextField(
-              controller: _eventName,
-              decoration: InputDecoration(hintText: 'Enter Event name'),
+      child: Container(
+        color: Colors.black,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                FlatButton(
+                    onPressed: () {
+                      DatePicker.showDateTimePicker(context, showTitleActions: true, minTime: DateTime.now(), maxTime: DateTime(2222, 6, 7), onChanged: (date) {
+                        print('change $date');
+                      }, onConfirm: (date) {
+                        setState(() {
+                          this.startTime = date;
+                        });
+                      }, currentTime: DateTime.now(), locale: LocaleType.en);
+                    },
+                    child: Text(
+                      'Event Start Time',
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                    )),
+                Text(
+                  '$startTime',
+                  style: TextStyle(color: Colors.redAccent, fontSize: 18),
+                ),
+              ],
             ),
-          ),
-          RaisedButton(
-              child: Text(
-                'Insert Event',
+            SizedBox(height: 20,),
+            Row(
+              children: <Widget>[
+                FlatButton(
+                    onPressed: () {
+                      DatePicker.showDateTimePicker(context, showTitleActions: true, minTime: DateTime(2019, 3, 5), maxTime: DateTime(2200, 6, 7),
+                          onChanged: (date) {
+                        print('change $date');
+                      }, onConfirm: (date) {
+                        setState(() {
+                          this.endTime = date;
+                        });
+                      }, currentTime: DateTime.now(), locale: LocaleType.en);
+                    },
+                    child: Text(
+                      'Event End Time',
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                    )),
+                Text(
+                  '$endTime',
+                  style: TextStyle(color: Colors.redAccent, fontSize: 18),
+                ),
+              ],
+            ),
+            SizedBox(height: 20,),
+            Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(40)), border: Border.all(width: 3, color: Colors.redAccent, style: BorderStyle.solid)),
+              padding: EdgeInsets.all(5.0),
+              child: TextField(
+                textAlign: TextAlign.center,
+                textInputAction: TextInputAction.send,
+                controller: _eventName,
+                decoration: InputDecoration(
+                  hintText: 'Enter Event title',
+                  hintStyle: TextStyle(color: Colors.redAccent, fontSize: 18),
+                ),
+                cursorColor: Colors.redAccent,
+                cursorRadius: Radius.circular(16.0),
+                cursorWidth: 16.0,
               ),
-              color: Colors.grey,
-              onPressed: () {
-                //log('add event pressed');
-                calendarClient.insert(
-                  _eventName.text,
-                  startTime,
-                  endTime,
-                );
-              }),
-        ],
+            ),
+            SizedBox(height: 20,),
+            RaisedButton(
+                child: Text(
+                  'Insert Event',
+                  style: TextStyle(color: Colors.black, fontSize: 20),
+                ),
+                color: Colors.redAccent,
+                onPressed: () {
+                  calendarClient.insert(
+                    _eventName.text,
+                    startTime,
+                    endTime,
+                  );
+                }),
+          ],
+        ),
       ),
     );
   }
